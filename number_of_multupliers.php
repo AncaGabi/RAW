14546bc2f5126175652e6d4c0b92ecfcf10f36c5
AncaGabi <92234507+AncaGabi@users.noreply.github.com>
2021-12-05 18:56:17 +0100

    Update number_of_multupliers.php
    Treated nonnumeric values and added more cases

--- number_of_multupliers.php
+++ number_of_multupliers.php
@@ -1,8 +1,9 @@
 <?php
-echo "<h4> Calculating the number of values in an array that are a multiplier of either 4 or 6 (or both)</h4><br>";
+echo "<h4> Calculate the number of values in an array that are a multiplier of either 4 or 6 (or both)</h4><br>";
 
 //create function
 function calculateNumberOfMultipliers($arrayOfNumbers){
+
 
 	//calculte the number of elements in the array
     $arrlength = count($arrayOfNumbers);  
@@ -13,41 +14,89 @@ function calculateNumberOfMultipliers($arrayOfNumbers){
 	for($i = 0; $i < $arrlength; $i++) {
 		echo "$arrayOfNumbers[$i]   ";
 		
-        //if the element of the array can be divided by 4 (the rest of the division is 0), count it (incrementation)
-        if($arrayOfNumbers[$i]%4==0)
-			{
-			$numberOfMultipliers++;
-            }
+        /*if the element of the array is numeric and can be divided by 4 (the rest of the division is 0), count it (incrementation)*/
+        if(is_numeric($arrayOfNumbers[$i])) {
+    	  	
+         	if($arrayOfNumbers[$i]%4==0) {
+				$numberOfMultipliers++;
+            	}
             
-        /* else (only if it isn't already divisible by 4) if the element of the array can be divided by 6, count it */
-    	elseif($arrayOfNumbers[$i]%6==0)
-			{
-			$numberOfMultipliers++;
-            }
+      		/* else (only if it isn't already divisible by 4) if the element of the array is numeric and can be divided by 6, count it */
+    		elseif($arrayOfNumbers[$i]%6==0) {
+				$numberOfMultipliers++;
+           		}
+             
+   		 }
     }
 	echo "<br>";
     
     
     //print the number of values in an array that are a multiplier of either 4 or 6 (or both)
-    echo "<b>The number of values in an array that are a multiplier of either 4 or 6 (or both):</b> $numberOfMultipliers 	 <br>";
+    if($numberOfMultipliers!=0) {
+        echo "<b>The number of values in an array that are a multiplier of either 4 or 6 (or both):</b> $numberOfMultipliers <br>";
+        }
+   	else {
+        echo "<b>The number of values in an array that are a multiplier of either 4 or 6 (or both):</b> 0 	 <br>";
+        }
+            
+            
+           
 }    
  
- 
 //define array  
-$arrayOfNumbers= array(4,15,12,19,35,30,24,444,600);
+$arrayOfNumbers= array(6, 18.0, -36);
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers);
+
+//define array  
+$arrayOfNumbers= array(4, -16, 0.20);
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers);
+
+
+//define array  
+$arrayOfNumbers= array(0,12, -60, 600.24);
 //call function
 calculateNumberOfMultipliers($arrayOfNumbers);
 
 
 //define array 
-$arrayOfNumbers= array(4,6,24,15,4);
+$arrayOfNumbers= array(0,6,13,c,hyte,-21,25,36,40,01,60,66,44,40,48,50,60,66,72,76,80,84,92,-500,320.06,120,160,180,abv,220,320,800,888," ",null,4,15,12,19,35,30,24,444,600,alabala,6,13,25,36,40,01,60,66,44,40,48,50,60,66,72,76,80,84,92,-500,320.06,120,160,180,abv,220,320,800,888," ",null, 4,15,12,19,35,30,24,444,600,alabala);
 //call function
 calculateNumberOfMultipliers($arrayOfNumbers);
 
 
-//define array 
-$arrayOfNumbers= array(6,13,25,36,40,60,66,44,40,48,50,60,66,72,76,80,84,92,120,160,180,220,320,800,888);
+//define array  
+$arrayOfNumbers= array(1, 5.1, -379);
 //call function
 calculateNumberOfMultipliers($arrayOfNumbers);
+
+//define array  
+$arrayOfNumbers= array(NULL);
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers);
+
+//define array  
+$arrayOfNumbers= array("  ");
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers); 
+
+//define array  
+$arrayOfNumbers= array(abv3);
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers); 
+
+
+//define array  
+$arrayOfNumbers= array(true);
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers); 
+
+
+//define array  
+$arrayOfNumbers= array("/&^%");
+//call function
+calculateNumberOfMultipliers($arrayOfNumbers); 
+
 ?> 
 
